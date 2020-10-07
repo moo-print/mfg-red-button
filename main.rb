a7569565ae96b862619aac484754d808421de044
John Peter Harvey <johnh@moo.com>
2020-10-07 18:39:52 +0100

    Started with call working

--- main.rb
+++ main.rb
@@ -1,18 +1,35 @@
 require 'cli/ui'
 require 'aws-sdk-lambda'
 
+class AWS
+    def get_status(lambda_client)
+        lambda_client.list_functions().filter_map do |response|
+            response.functions.filter_map do |function|
+                tag_response = lambda_client.list_tags({resource: function.function_arn})
+                if (tag_response.tags['Project'] == "moo-mfg" && tag_response.tags['Environment'] == "dev")
+                    function.function_name
+                else
+                    nil
+                end
+            end
+        end
+    end
+end
+
 # Init the UI
 CLI::UI::StdoutRouter.enable
 
 # Configure the AWS connection
 lambda_client = Aws::Lambda::Client.new
-lambda_list << lambda_client.list_functions().each do |response|
-    response.functions.each do |function|
-        function.function_name
-    end
-end
+
+# Get env
+environment = CLI::UI.ask("Environment?", default: "dev")
+lambdas = []
 
 # Display UI
-CLI::UI::Frame.open('Production Status') do
-    puts lambda_list
+CLI::UI::Frame.open("#{environment} Status") do
+    sg = CLI::UI::SpinGroup.new
+    sg.add('Fetching Lambdas...') { |spinner| lambdas = AWS.new.get_status(lambda_client); spinner.update_title('Fetched Lambas'); }
+    sg.wait
+    puts lambdas
 end
